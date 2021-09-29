8a46d5de8a6b534bebb16dc0011b01472e2331fe
Vadym Liubko <neznam.ua@gmail.com>
2021-09-30 02:15:36 +0300

    try to deploy to cloud run

--- ops/terraform/api-gateway.tf
+++ ops/terraform/api-gateway.tf
@@ -2,4 +2,9 @@ resource "google_api_gateway_api" "api" {
   provider = google-beta
   api_id = "api"
   display_name = "API Gateway"
+
+  labels = {
+    author = "vliubko"
+    service = "oms-lite"
+  }
 }
