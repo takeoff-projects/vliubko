0733879fc1c3d196b2a937f7c370a6c3827dd169
Vadym Liubko <neznam.ua@gmail.com>
2021-09-30 19:11:44 +0300

    start playing with CORS

--- ops/terraform/api-gateway.tf
+++ ops/terraform/api-gateway.tf
@@ -61,5 +61,5 @@ resource "google_api_gateway_gateway" "api_gw" {
 
   provider = google-beta
   api_config = google_api_gateway_api_config.api_gw.id
-  gateway_id = "${var.app_name}-gw"
+  gateway_id = "${var.app_name}"
 }
