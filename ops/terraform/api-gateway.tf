8a8bed9fe5977ef5e762b48851c73ad5ac948064
Vadym Liubko <neznam.ua@gmail.com>
2021-09-30 17:19:20 +0300

    deploy API GW

--- ops/terraform/api-gateway.tf
+++ ops/terraform/api-gateway.tf
@@ -1,24 +1,43 @@
+module "api_gw_sa" {
+  source     = "terraform-google-modules/service-accounts/google"
+  version    = "4.0.3"
+  project_id = var.google_project_id
+  names      = ["api-gw"]
+  project_roles = [
+    "${var.google_project_id}=>roles/run.invoker",
+  ]
+}
+
 resource "google_api_gateway_api" "api" {
+  project = var.google_project_id
+
   provider     = google-beta
   api_id       = "api"
   display_name = "API Gateway"
 
   labels = {
     author  = "vliubko"
-    service = "oms-lite"
+    service = var.app_name
   }
 }
 
 resource "google_api_gateway_api_config" "api_gw" {
+  project = var.google_project_id
+
   provider = google-beta
   api = google_api_gateway_api.api.api_id
-  api_config_id = "config"
+  api_config_id_prefix = var.app_name
 
-  # gateway_config {
-  #   backend_config {
-  #     google_service_account = google_service_account.api_gateway.email
-  #   }
-  # }
+  labels = {
+    author  = "vliubko"
+    service = var.app_name
+  }
+
+  gateway_config {
+    backend_config {
+      google_service_account = module.api_gw_sa.email
+    }
+  }
 
   openapi_documents {
     document {
@@ -38,6 +57,8 @@ resource "google_api_gateway_api_config" "api_gw" {
 }
 
 resource "google_api_gateway_gateway" "api_gw" {
+  project = var.google_project_id
+
   provider = google-beta
   api_config = google_api_gateway_api_config.api_gw.id
   gateway_id = "${var.app_name}-gw"
