a96099234fbdb6f0b0fe4baf4bdedf43dc93620d
Vadym Liubko <neznam.ua@gmail.com>
2021-09-30 05:17:56 +0300

    use gcloud beta to enable secrets in cloud run

--- business/sys/database/client.go
+++ business/sys/database/client.go
@@ -3,12 +3,14 @@ package omslitedb
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/lib/pq"
 )
 
 // InitDB sets up setting up the connection pool global variable.
 func InitDB(dataSource string) (*Queries, error) {
+	log.Printf("Connectiong to the DB with connection string: %s\n", dataSource)
 	db, err := sql.Open("postgres", dataSource)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't connect to the postgres! %s", err.Error())
